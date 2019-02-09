1b8b24efc9d003aa0f1e3d64012fa0fb36b07952
Hydra10641 <47456390+Hydra10641@users.noreply.github.com>
2019-02-09 12:14:44 -0300

    teste de criação de metodos para as funções de andar, setando o proprio servo nesse metodo

---
+++ TeamCode/src/main/java/models/testCollectArm.java
@@ -0,0 +1,13 @@
+package models;
+
+import models.collectArm;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class testCollectArm extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+    }
+
+}
+
