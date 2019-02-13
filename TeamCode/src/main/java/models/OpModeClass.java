94c5682e06495a47247f3533f264bc232307d174
Hydra10641 <47456390+Hydra10641@users.noreply.github.com>
2019-02-13 14:11:10 -0300

    Update OpModeClass.java

--- TeamCode/src/main/java/models/OpModeClass.java
+++ TeamCode/src/main/java/models/OpModeClass.java
@@ -5,17 +5,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.Range;
 
-
 public class OpModeClass extends LinearOpMode{
 
     @Override
     public void runOpMode() throws InterruptedException {
         /*Here we declare the devices of our robot (servos, motors and sensors)*/
-        Arms servos = new Arms();
-        servos.arms(hardwareMap.get(Servo.class, "servoDepositArmHand"),
-                     hardwareMap.get(Servo.class, "servoDepositArmExpansion"),
-                     hardwareMap.get(Servo.class, "servoColectArmHand"),
-                     hardwareMap.get(Servo.class, "servoColectArmExpansion"));
+        Arms servos = new Arms(hardwareMap.get(Servo.class, "servoDepositArmHand"),
+                hardwareMap.get(Servo.class, "servoDepositArmExpansion"),
+                hardwareMap.get(Servo.class, "servoColectArmHand"),
+                hardwareMap.get(Servo.class, "servoColectArmExpansion"));
 
         Omni omniWheels = new Omni();
         omniWheels.omni(hardwareMap.get(DcMotor.class, "leftFrontWheel"),
@@ -26,5 +24,15 @@ public class OpModeClass extends LinearOpMode{
         NormalWheels normalWheels = new NormalWheels();
         normalWheels.normalWheels(hardwareMap.get(DcMotor.class, "leftWheel"),
                                   hardwareMap.get(DcMotor.class, "rightWheel"));
+
+        Locomotion motors = new Locomotion();
+        motors.locomotion(  hardwareMap.get(DcMotor.class, "leftFrontWheel" ),
+                            hardwareMap.get(DcMotor.class, "leftBackWheel"  ),
+                            hardwareMap.get(DcMotor.class, "RightFrontWheel"),
+                            hardwareMap.get(DcMotor.class, "RightBackWheel"));
+
+        motors.setForwardBackward(gamepad1.left_stick_y);
+        motors.setTurnAtSameAxis(gamepad1.left_stick_x);
+        motors.setTurn(gamepad1.left_stick_x, -gamepad1.left_stick_y);
     }
 }
