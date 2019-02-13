8cd2da0fdcd2a276ae10466e60b18f2bd6b2e4a8
Hydra10641 <47456390+Hydra10641@users.noreply.github.com>
2019-02-13 14:11:04 -0300

    Update EncoderClass.java

--- TeamCode/src/main/java/models/EncoderClass.java
+++ TeamCode/src/main/java/models/EncoderClass.java
@@ -2,11 +2,17 @@ package models;
 
 public class EncoderClass {
 
-    public void cmToCounts(double countsPerMotor, double driveGearReduction, double wheelDiameterCm, double countsPerCm){
-        countsPerMotor    = 288 ; // Contagens do motor qs
-        driveGearReduction    = 2.0 ; // Redução?
-        wheelDiameterCm = 4.0 ; // Diametro da roda
-        countsPerCm = (countsPerMotor * driveGearReduction) /
-                          (wheelDiameterCm * 3.1415);
+    private int countsToCm;
+
+    public void cmToCounts(double countsPerMotor, double driveGearReduction, double wheelDiameterCm, double countsPerCm, int countsToCm){
+        countsPerMotor      = 288 ; // Contagens do motor qs
+        driveGearReduction  = 2.0 ; // Redução?
+        wheelDiameterCm     = 4.0 ; // Diametro da roda
+        countsPerCm = (countsPerMotor * driveGearReduction) / (wheelDiameterCm * 3.1415);
+        this.countsToCm = (int) Math.round(countsPerCm);
+    }
+
+    public int getCountsToCm() {
+        return countsToCm;
     }
 }
