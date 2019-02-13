04cc33e99c6ecaeffa6dc99e59fefd11663f5a6d
Hydra10641 <47456390+Hydra10641@users.noreply.github.com>
2019-02-13 15:19:41 -0300

    Update OpModeClass.java

--- TeamCode/src/main/java/models/OpModeClass.java
+++ TeamCode/src/main/java/models/OpModeClass.java
@@ -9,30 +9,23 @@ public class OpModeClass extends LinearOpMode{
 
     @Override
     public void runOpMode() throws InterruptedException {
-        /*Here we declare the devices of our robot (servos, motors and sensors)*/
+        /*Here we declare the devices of our robot (servos, motors and sensors)
+         *You can choose between the Omni locomotion system (Omni)
+         *Or the locomotion system with two traction engines (NormalWheels)
+         */
+
         Arms servos = new Arms(hardwareMap.get(Servo.class, "servoDepositArmHand"),
                 hardwareMap.get(Servo.class, "servoDepositArmExpansion"),
                 hardwareMap.get(Servo.class, "servoColectArmHand"),
                 hardwareMap.get(Servo.class, "servoColectArmExpansion"));
 
-        Omni omniWheels = new Omni();
-        omniWheels.omni(hardwareMap.get(DcMotor.class, "leftFrontWheel"),
-                        hardwareMap.get(DcMotor.class, "leftBackWheel"),
-                        hardwareMap.get(DcMotor.class, "rightFrontWheel"),
-                        hardwareMap.get(DcMotor.class, "rightBackWheel"));
+        Omni omniWheels = new Omni(hardwareMap.get(DcMotor.class, "leftFrontWheel"),
+                hardwareMap.get(DcMotor.class, "leftBackWheel"),
+                hardwareMap.get(DcMotor.class, "rightFrontWheel"),
+                hardwareMap.get(DcMotor.class, "rightBackWheel"));
 
-        NormalWheels normalWheels = new NormalWheels();
-        normalWheels.normalWheels(hardwareMap.get(DcMotor.class, "leftWheel"),
-                                  hardwareMap.get(DcMotor.class, "rightWheel"));
+        /*NormalWheels normalWheels = new NormalWheels(hardwareMap.get(DcMotor.class, "leftWheel"),
+                hardwareMap.get(DcMotor.class, "rightWheel"));*/
 
-        Locomotion motors = new Locomotion();
-        motors.locomotion(  hardwareMap.get(DcMotor.class, "leftFrontWheel" ),
-                            hardwareMap.get(DcMotor.class, "leftBackWheel"  ),
-                            hardwareMap.get(DcMotor.class, "RightFrontWheel"),
-                            hardwareMap.get(DcMotor.class, "RightBackWheel"));
-
-        motors.setForwardBackward(gamepad1.left_stick_y);
-        motors.setTurnAtSameAxis(gamepad1.left_stick_x);
-        motors.setTurn(gamepad1.left_stick_x, -gamepad1.left_stick_y);
     }
 }
