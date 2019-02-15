083bcb044d8fba2c212f069a4361cfb8d48e6ea7
Hydra10641 <47456390+Hydra10641@users.noreply.github.com>
2019-02-15 17:50:36 -0300

    Mudança no nome da opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HydraOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HydraOpMode.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-public class OpModeClass extends LinearOpMode {
+public class HydraOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         /*Here we declare the devices of our robot (servos, motors and sensors)
